2203f1fdb682150178f69907c13ee97c82fd7280
Your Name <you@example.com>
2024-10-03 01:01:15 +0700

    pemilihan tahun kabisat

---
+++ pemilihan2percobaan108.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class pemilihan2percobaan108 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan tahun: ");
+        int angka = sc.nextInt();
+
+        if (angka % 4 == 0){
+            if (angka % 100 != 0){
+                System.out.println("Tahun kabisat");
+            }
+        }
+        else {
+            System.out.println("Bukan tahun kabisat");
+        }
+
+    }
+
+}
