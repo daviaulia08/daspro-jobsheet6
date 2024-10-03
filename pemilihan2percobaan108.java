a220add6937a4507b819ac8c76ccdb0dda6512ed
Your Name <you@example.com>
2024-10-03 07:37:29 +0700

    perubahan tahun 2100

--- pemilihan2percobaan108.java
+++ pemilihan2percobaan108.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 public class pemilihan2percobaan108 {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner input08 = new Scanner(System.in);
 
         System.out.print("Masukkan tahun: ");
-        int angka = sc.nextInt();
+        int angka = input08.nextInt();
 
         if (angka % 4 == 0){
-            System.out.println("Tahun kabisat");
+            if (angka % 100 != 0){
+                System.out.println("Tahun kabisat");
+            }
+            else {
+                System.out.println("bukan tahun kabisat");
+            }
+
         }
         else {
             System.out.println("Bukan tahun kabisat");
@@ -17,3 +23,4 @@ public class pemilihan2percobaan108 {
     }
 
 }
+
